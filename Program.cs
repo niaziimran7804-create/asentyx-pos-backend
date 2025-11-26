ba3ba7f7f120eff8c7d67a3439633d1714110e2c
Akhlaq <muhammad.akhlaq@edufi.tech>
2025-11-27 03:41:38 +0500

    set dev

--- Program.cs
+++ Program.cs
@@ -114,12 +114,11 @@ using (var scope = app.Services.CreateScope())
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+{  
+  app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors("AllowAngularApp");
 app.UseAuthentication();
 app.UseAuthorization();
